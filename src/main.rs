2eb426dc2b3be6a3be7a6d608ae3474c5cb4a865
CatFoxMiao <kindonefox@qq.com>
2025-05-28 18:36:27 +0800

    add token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        1 => {
+            //REPL
+            println!("no file");
+        }
+        2 => {
+            println!("a file");
+            // 执行文件
+        }
+        _ => {
+            println!("usage: jlox[script]");
+        }
+    }
 }
---
+++ src/token.rs
@@ -0,0 +1,63 @@
+pub enum Literal {
+    String(String),
+    Number(f64),
+    Bool(bool),
+    None,
+}
+
+enum TokenType {
+    // Single-character tokens.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF,
+}
+
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexeme: String,
+    pub literal: Literal,
+    pub line: uszie,
+}
